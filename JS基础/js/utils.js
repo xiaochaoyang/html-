abf818ede8c427fa865c0b77e0f7dc3cd7524983
肖朝阳 <534600451@qq.com>
2019-08-03 22:28:07 +0800

    js

---
+++ JS基础/js/utils.js
@@ -0,0 +1,26 @@
+
+function $(id) {
+    return document.getElementById(id);
+}
+
+function getElementsClassName(obj, className) {
+    if (obj.getElementsByClassName) {
+        return obj.getElementsByClassName(className);
+    } else {
+        var list = obj.getElementsByTagName('*');
+        var arr = [];
+        for (var i = 0; i < list.length; i++) {
+            var ele = list[i];
+            if (ele.className === className) {
+                arr.push(ele);
+            }
+        }
+        return arr;
+    }
+}
+
+function each(obj, callback) {
+    for (var i = 0; i < obj.length; i++) {
+        callback(obj[i],i);
+    }
+}
